8df940526cc958ac4ace4dfedbe1eb02020c6e0c
keylin02 <keydalorso@gmail.com>
2024-05-20 13:20:09 -0600

    tarea 1 progra 2

---
+++ appValores/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appValores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = { 1, 2, 3, 4, 5 };
+            int suma = 0;
+            
+            for (int i = 0; i < valores.Length; i++)
+            {
+                suma = suma + valores[i];  
+            }
+
+            Console.WriteLine(suma);
+            Console.ReadKey();
+        }
+    }
+}
